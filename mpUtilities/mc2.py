76ff010575bf5a7daa9b93426981784f09c65ae5
Graham Arden <graham@trigfa.org.uk>
2015-11-06 12:13:17 +0000

    should now work for py3

--- mpUtilities/mc2.py
+++ mpUtilities/mc2.py
@@ -4,10 +4,10 @@
 
 
 class mc2:
-        
+
     """Defines an mc2 class with the required attributes"""
-    
-    def __init__(self, measdate, linac, modality, inplane_axis, crossplane_axis, depth_axis, inplane_axis_dir, crossplane_axis_dir, depth_axis_dir, energy, ssd, field_inplane, field_crossplane, scan_curvetype, scan_depth, scan_offaxis_inplane, scan_offaxis_crossplane, meas_time, meas_unit, xdata, ydata, refdata, comment):         
+
+    def __init__(self, measdate, linac, modality, inplane_axis, crossplane_axis, depth_axis, inplane_axis_dir, crossplane_axis_dir, depth_axis_dir, energy, ssd, field_inplane, field_crossplane, scan_curvetype, scan_depth, scan_offaxis_inplane, scan_offaxis_crossplane, meas_time, meas_unit, xdata, ydata, refdata, comment):
         self.measdate = measdate
         self.comment = comment
         self.linac = linac
@@ -27,31 +27,31 @@ class mc2:
         self.scan_offaxis_inplane = scan_offaxis_inplane
         self.scan_offaxis_crossplane = scan_offaxis_crossplane
         self.meas_time = meas_time
-        self.meas_unit = meas_unit      
+        self.meas_unit = meas_unit
         self.xdata = xdata
         self.ydata = ydata
         self.refdata = refdata
 
 def read_mc2(file, datastartline, dataendline):
-        
-    """Read data from the mc2 file and present it as an instance of the mc2 class   
+
+    """Read data from the mc2 file and present it as an instance of the mc2 class
     As files can contain more than one dataset the start and end line numbers
     need to be supplied.
-    These can be obtained using datasetinfo(file)   
-    Use as dataset = read_mc2(file, datastartline, dataendline)"""  
-    
+    These can be obtained using datasetinfo(file)
+    Use as dataset = read_mc2(file, datastartline, dataendline)"""
+
     import numpy
-    
+
     lineNumber = 0
     dataline = 0
-    ifile = open(file, 'r') 
+    ifile = open(file, 'r')
     lines = ifile.readlines()
     linac="N/A"  # earlier vesions of the mc2 software didn't include this field, if its missing then N/A will be returned instead
     comment="N/A" # the field COMMENT is only present if a comment has acually been added
-    for line in lines: 
+    for line in lines:
         line = line.replace('\t', ',')  # replaces tabs with commas
         line = line.rstrip('\r\n')         # strips control characters from the end of the line
-        
+
         if (lineNumber > datastartline) and (lineNumber < dataendline):
             if "MEAS_DATE" in line:
                 measdate = (line.split("=")[1])
@@ -59,59 +59,59 @@ def read_mc2(file, datastartline, dataendline):
                 comment = (line.split("=")[1])
             if "LINAC" in line:
                 linac = (line.split("=")[1])
-                                
+
             if "MODALITY=" in line:
                 modality = (line.split("=")[1])
-                
+
             if "INPLANE_AXIS=" in line:
                 inplane_axis = (line.split("=")[1])
-                
+
             if "CROSSPLANE_AXIS=" in line:
                 crossplane_axis = (line.split("=")[1])
 
             if "DEPTH_AXIS=" in line:
                 depth_axis = (line.split("=")[1])
-        
+
             if "INPLANE_AXIS_DIR=" in line:
                 inplane_axis_dir = (line.split("=")[1])
-                
+
             if "CROSSPLANE_AXIS_DIR=" in line:
                 crossplane_axis_dir = (line.split("=")[1])
-                
+
             if "DEPTH_AXIS_DIR=" in line:
                 depth_axis_dir = (line.split("=")[1])
-                
+
             if "ENERGY" in line:
                 energy = (int((line.split("=")[1]).split('.')[0])) # converts from text to an integer
-                
+
             if "SSD=" in line:
                 ssd = (int((line.split("=")[1]).split('.')[0])) # converts from text to an integer
-                
-        
+
+
             if ("FIELD_INPLANE" in line) and ("REF" not in line):
                 field_inplane=(int((line.split("=")[1]).split('.')[0]))# converts from text to an integer
-        
+
             if  ("FIELD_CROSSPLANE" in line) and ("REF" not in line):
                 field_crossplane = (int((line.split("=")[1]).split('.')[0])) # converts from text to an integer
-                
+
             if "SCAN_CURVETYPE" in line:
                 scan_curvetype = (line.split("=")[1])
-                
+
             if "SCAN_DEPTH=" in line:
                 scan_depth  = (int((line.split("=")[1]).split('.')[0])) # converts from text to an integer
-        
+
             if "SCAN_OFFAXIS_INPLANE=" in line:
                 scan_offaxis_inplane = (int((line.split("=")[1]).split('.')[0])) # converts from text to an integer
-                
+
             if "SCAN_OFFAXIS_CROSSPLANE=" in line:
                 scan_offaxis_crossplane = (int((line.split("=")[1]).split('.')[0])) # converts from text to an integer
-                
+
             if "MEAS_TIME=" in line:
                 meas_time = (line.split("=")[1])
-                
+
             if "MEAS_UNIT=" in line:
                 meas_unit = (line.split("=")[1])
-           
+
             if line.startswith(',,,'):     # this must be our data
                 if dataline == 0:
                     xvalue, yvalue, refvalue = extractdata(line)
@@ -127,24 +127,24 @@ def read_mc2(file, datastartline, dataendline):
                     ydata = numpy.hstack([ydata, yvalue])
                     refdata = numpy.hstack([refdata, refvalue])
                 dataline = dataline + 1
-                
+
             if "END_DATA" in line:   #we have now reached the end of aour data and can create our class
                 try:
                     scan_depth
                 except NameError:
                     scan_depth="0"
-                    
-                mc2_dataset = mc2(measdate, linac, modality, inplane_axis, crossplane_axis, depth_axis, inplane_axis_dir, crossplane_axis_dir, depth_axis_dir, energy, ssd, field_inplane, field_crossplane, scan_curvetype, scan_depth, scan_offaxis_inplane, scan_offaxis_crossplane, meas_time, meas_unit, xdata, ydata, refdata, comment)   
-                
+
+                mc2_dataset = mc2(measdate, linac, modality, inplane_axis, crossplane_axis, depth_axis, inplane_axis_dir, crossplane_axis_dir, depth_axis_dir, energy, ssd, field_inplane, field_crossplane, scan_curvetype, scan_depth, scan_offaxis_inplane, scan_offaxis_crossplane, meas_time, meas_unit, xdata, ydata, refdata, comment)
+
         lineNumber = lineNumber + 1
-        
-    return mc2_dataset      
-    
+
+    return mc2_dataset
+
 
 def open_mcc(dataDirectory):
-        
+
     """A really simple function which presents a GUI to allow the user to select a file"""
-    
+
     import Tkinter, tkFileDialog
     root = Tkinter.Tk()
     root.withdraw()
@@ -153,11 +153,11 @@ def open_mcc(dataDirectory):
 
 
 def extractdata(line):
-        
+
     """For each line, return the x and y values, check whether there is reference value
     and if so return the reference value, otherwise return a reference      value of 1 """
-    
-    newArray = (line.split(','))  # 
+
+    newArray = (line.split(','))  #
     if len(newArray) == 8:
         # convert the strings to floats
         xvalue = float(newArray[3])
@@ -171,54 +171,54 @@ def extractdata(line):
         refvalue = 1
         return xvalue, yvalue, refvalue
     else:
-        print "Houston, we have a problem, This line does not appear to be data!:"
-        print line
+        print("Houston, we have a problem, This line does not appear to be data!:")
+        print(line)
 
 
 def datasetinfo(file):
-        
+
     """Finds datasets within a file and returns some useful info so you can decide
     which one you need
     Returns the start and end lines, acquisition date, energy,field size, direction and depth
     Call as 'DataStart, DataEnd, MeasDate, Energy, FieldSize, Depth=datasetinfo(inputfile)' """
-    
+
     datasets = 0
-    ifile = open(file, 'r') 
+    ifile = open(file, 'r')
     lines = ifile.readlines()
     datasets = 0
-    lineNumber = 0  
+    lineNumber = 0
     BeginScan=[]; EndScan=[]; MeasDate = []; Energy = []; FieldSize=[]; Depth=[]; Direction=[]
     for line in lines:
-            
+
         line = line.replace('\t', ',')      # replaces all the tabs with commas
         line = line.rstrip('\r\n')            # strips any control characters from the end of the line
-        
+
         if ("BEGIN_SCAN" in line) and ("DATA" not in line):
             BeginScan.append(lineNumber)
             datasets = datasets+1
-            
+
         if "MEAS_DATE" in line:
             MeasDate.append(line.split("=")[1])
-            
+
         if "ENERGY" in line:
             Energy.append(int((line.split("=")[1]).split('.')[0]))
-            # This rather convoluted line extracts the integer from a string 
+            # This rather convoluted line extracts the integer from a string
             # e.g the string "6.00" is converted to the integer "6"
             #  Energy=(int(line.split('.')[0]))
 
         if "SCAN_DEPTH=" in line:
             Depth.append(int((line.split("=")[1]).split('.')[0]))
-            
+
         if ("FIELD_INPLANE" in line) and ("REF" not in line):
             FieldSize.append((int((line.split("=")[1]).split('.')[0]))/10) # convert from mm to cm
-        3               
+        3
         #
         # This probably shouldn't be relied upon as the direction
         # in the old data appears to be quite unreliable.
         # The filename appears to be a more reliable guide to the scan direction!
         #
-        
-        if "SCAN_CURVETYPE=" in line:                   
+
+        if "SCAN_CURVETYPE=" in line:
             answer = (line.split("=")[1])
             if (line.split("=")[1]) == "CROSSPLANE_PROFILE":
                 Direction.append("AB(X)")
@@ -229,13 +229,7 @@ def datasetinfo(file):
                 Depth.append("N/A")
 
         if ("END_SCAN" in line) and ("DATA" not in line):
-            EndScan.append(lineNumber)      
-            
-        lineNumber = lineNumber + 1             
+            EndScan.append(lineNumber)
+
+        lineNumber = lineNumber + 1
     return BeginScan, EndScan, MeasDate, Energy, FieldSize, Depth, Direction
-
-
-
-
-    
-
