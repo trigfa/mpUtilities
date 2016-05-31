08c74557d59f8a04961ceda312e93a8755fd7661
Graham Arden <graham@trigfa.org.uk>
2016-05-31 15:33:28 +0100

    added qcw_to_df function

--- mpUtilities/webline.py
+++ mpUtilities/webline.py
@@ -5,6 +5,7 @@
 import csv
 from lxml import etree
 import dateutil.parser
+import pandas as pd
 
 class weblinedata:
 
@@ -220,3 +221,115 @@ def csv2webline(filename):
         linenumber = linenumber+1
     webline_dataset = weblinedata(date, linac, modality, energy, CAX, flatness, symmetryGT, symmetryAB)
     return webline_dataset
+
+def qcw_to_df(filename):
+    """Read data from a qcw file and returns a Pandas DataFrame
+    Use as dataset = qcw_to_df(file)"""
+
+    # the dictionary 'Parameters' contains the location of each parameter in our xml file
+    Parameters = {"TreatmentUnit":("Worklist/AdminData/AdminValues/TreatmentUnit"),
+              "Modality":("Worklist/AdminData/AdminValues/Modality"),
+              "Energy":("Worklist/AdminData/AdminValues/Energy"),
+              "FieldSize":("Worklist/AdminData/AdminValues/Fieldsize"),
+              "SDD":("Worklist/AdminData/AdminValues/SDD"),
+              "Gantry":("Worklist/AdminData/AdminValues/Gantry"),
+              "Wedge":("Worklist/AdminData/AdminValues/Wedge"),
+              "MU":("Worklist/AdminData/AdminValues/MU"),
+              "My":("Worklist/AdminData/AdminValues/My"),
+              "AnalyzeParametersCAXMin":("Worklist/AdminData/AnalyzeParams/CAX/Min"),
+              "AnalyzeParametersCAXMax":("Worklist/AdminData/AnalyzeParams/CAX/Max"),
+              "AnalyzeParametersCAXTarget":("Worklist/AdminData/AnalyzeParams/CAX/Target"),
+              "AnalyzeParametersCAXNorm":("Worklist/AdminData/AnalyzeParams/CAX/Norm"),
+              "AnalyzeParametersFlatnessMin":("Worklist/AdminData/AnalyzeParams/Flatness/Min"),
+              "AnalyzeParametersFlatnessMax":("Worklist/AdminData/AnalyzeParams/Flatness/Max"),
+              "AnalyzeParametersFlatnessTarget":("Worklist/AdminData/AnalyzeParams/Flatness/Target"),
+              "AnalyzeParametersFlatnessNorm":("Worklist/AdminData/AnalyzeParams/Flatness/Norm"),
+              "AnalyzeParametersSymmetryGTMin":("Worklist/AdminData/AnalyzeParams/SymmetryGT/Min"),
+              "AnalyzeParametersSymmetryGTMax":("Worklist/AdminData/AnalyzeParams/SymmetryGT/Max"),
+              "AnalyzeParametersSymmetryGTTarget":("Worklist/AdminData/AnalyzeParams/SymmetryGT/Target"),
+              "AnalyzeParametersSymmetryGTNorm":("Worklist/AdminData/AnalyzeParams/SymmetryGT/Norm"),
+              "AnalyzeParametersSymmetryLRMin":("Worklist/AdminData/AnalyzeParams/SymmetryLR/Min"),
+              "AnalyzeParametersSymmetryLRMax":("Worklist/AdminData/AnalyzeParams/SymmetryLR/Max"),
+              "AnalyzeParametersSymmetryLRTarget":("Worklist/AdminData/AnalyzeParams/SymmetryLR/Target"),
+              "AnalyzeParametersSymmetryLRNorm":("Worklist/AdminData/AnalyzeParams/SymmetryLR/Norm"),
+              "AnalyzeParametersBQFMin":("Worklist/AdminData/AnalyzeParams/BQF/Min"),
+              "AnalyzeParametersBQFMax":("Worklist/AdminData/AnalyzeParams/BQF/Max"),
+              "AnalyzeParametersBQFTarget":("Worklist/AdminData/AnalyzeParams/BQF/Target"),
+              "AnalyzeParametersBQFNorm":("Worklist/AdminData/AnalyzeParams/BQF/Norm"),
+              "AnalyzeParametersWedgeMin":("Worklist/AdminData/AnalyzeParams/Wedge/Min"),
+              "AnalyzeParametersWedgeMax":("Worklist/AdminData/AnalyzeParams/Wedge/Max"),
+              "AnalyzeParametersWedgeTarget":("Worklist/AdminData/AnalyzeParams/Wedge/Target"),
+              "AnalyzeParametersWedgeNorm":("Worklist/AdminData/AnalyzeParams/Wedge/Norm"),
+              "MeasDataMeasValuesCAXValue":("MeasData/MeasValues/CAX/Value"),
+              "MeasDataMeasValuesCAXValid":("MeasData/MeasValues/CAX/Valid"),
+              "MeasDataMeasValuesG10Value":("MeasData/MeasValues/G10/Value"),
+              "MeasDataMeasValuesG10Valid":("MeasData/MeasValues/G10/Valid"),
+              "MeasDataMeasValuesL10Value":("MeasData/MeasValues/L10/Value"),
+              "MeasDataMeasValuesL10Valid":("MeasData/MeasValues/L10/Valid"),
+              "MeasDataMeasValuesT10Value":("MeasData/MeasValues/T10/Value"),
+              "MeasDataMeasValuesT10Valid":("MeasData/MeasValues/T10/Valid"),
+              "MeasDataMeasValuesR10Value":("MeasData/MeasValues/R10/Value"),
+              "MeasDataMeasValuesR10Valid":("MeasData/MeasValues/R10/Valid"),
+              "MeasDataMeasValuesG20Value":("MeasData/MeasValues/G20/Value"),
+              "MeasDataMeasValuesG20Valid":("MeasData/MeasValues/G20/Valid"),
+              "MeasDataMeasValuesL20Value":("MeasData/MeasValues/L20/Value"),
+              "MeasDataMeasValuesL20Valid":("MeasData/MeasValues/L20/Valid"),
+              "MeasDataMeasValuesT20Value":("MeasData/MeasValues/T20/Value"),
+              "MeasDataMeasValuesT20Valid":("MeasData/MeasValues/T10/Valid"),
+              "MeasDataMeasValuesR20Value":("MeasData/MeasValues/R20/Value"),
+              "MeasDataMeasValuesR20Valid":("MeasData/MeasValues/R20/Valid"),
+              "MeasDataMeasValuesE1Value":("MeasData/MeasValues/E1/Value"),
+              "MeasDataMeasValuesE1Valid":("MeasData/MeasValues/E1/Valid"),
+              "MeasDataMeasValuesE2Value":("MeasData/MeasValues/E2/Value"),
+              "MeasDataMeasValuesE2Valid":("MeasData/MeasValues/E2/Valid"),
+              "MeasDataMeasValuesE3Value":("MeasData/MeasValues/E3/Value"),
+              "MeasDataMeasValuesE3Valid":("MeasData/MeasValues/E3/Valid"),
+              "MeasDataMeasValuesE4Value":("MeasData/MeasValues/E4/Value"),
+              "MeasDataMeasValuesE4Valid":("MeasData/MeasValues/E4/Valid"),
+              "MeasDataMeasValuesTempValue":("MeasData/MeasValues/Temp/Value"),
+              "MeasDataMeasValuesTempValid":("MeasData/MeasValues/Temp/Valid"),
+              "MeasDataMeasValuesPressureValue":("MeasData/MeasValues/Pressure/Value"),
+              "MeasDataMeasValuesPressureValid":("MeasData/MeasValues/Pressure/Valid"),
+              "MeasDataMeasValuesCAXRateValue":("MeasData/MeasValues/CAXRate/Value"),
+              "MeasDataMeasValuesCAXRateValid":("MeasData/MeasValues/CAXRate/Valid"),
+              "MeasDataMeasValuesExpTimeValue":("MeasData/MeasValues/ExpTime/Value"),
+              "MeasDataMeasValuesExpTimeValid":("MeasData/MeasValues/ExpTime/Valid"),
+              "MeasDataAnalyzeValuesCAXValue":("MeasData/AnalyzeValues/CAX/Value"),
+              "MeasDataAnalyzeValuesCAXValid":("MeasData/AnalyzeValues/CAX/Valid"),
+              "MeasDataAnalyzeValuesFlatnessValue":("MeasData/AnalyzeValues/Flatness/Value"),
+              "MeasDataAnalyzeValuesFlatnessValid":("MeasData/AnalyzeValues/Flatness/Valid"),
+              "MeasDataAnalyzeValuesSymmetryGTValue":("MeasData/AnalyzeValues/SymmetryGT/Value"),
+              "MeasDataAnalyzeValuesSymmetryGTValid":("MeasData/AnalyzeValues/SymmetryGT/Valid"),
+              "MeasDataAnalyzeValuesSymmetryLRValue":("MeasData/AnalyzeValues/SymmetryLR/Value"),
+              "MeasDataAnalyzeValuesSymmetryLRValid":("MeasData/AnalyzeValues/SymmetryLR/Valid"),
+              "MeasDataAnalyzeValuesBQFValue":("MeasData/AnalyzeValues/BQF/Value"),
+              "MeasDataAnalyzeValuesBQFValid":("MeasData/AnalyzeValues/BQF/Valid"),
+              "MeasDataAnalyzeValuesWedgeValue":("MeasData/AnalyzeValues/Wedge/Value"),
+              "MeasDataAnalyzeValuesWedgeValid":("MeasData/AnalyzeValues/Wedge/Valid")
+             }
+    # open the and parse the datafile
+    xmlData = etree.parse(filename)
+    trendData = xmlData.findall("//TrendData")
+
+    # sort the directory keys
+    sortedKeys = list(sorted(Parameters.keys()))
+
+    # create a list of headers for our pandas dataframe
+    dateList=[]
+    dateList.append('date')
+    headers = dateList+sortedKeys
+    # creates any empty pandas dataframe with the same number of rows as the number of records in trendData
+    # and with the column headers set to 'headers'
+    df = pd.DataFrame(index=np.arange(0,len(trendData)), columns=headers)
+    for a,b in enumerate(trendData):
+        result={}
+        result["date"] = dateutil.parser.parse(b.attrib['date'])
+        for i,j in enumerate(Parameters):
+            result[j] = b.findtext(Parameters[j])
+            df.loc[a]=(result)
+    df = df.set_index('date')
+    #for i in int64list:
+    #    df.attrib(i) = pd.to_numeric(df.attrib(i))
+
+    # df = df.convert_objects(convert_numeric=True)
+    return df
